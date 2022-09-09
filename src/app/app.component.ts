09d78c3dba49e22aa2290f6582ba6391eb631679
John Andrés Camacho <jcamacho@hifebo.com>
2022-09-09 11:18:58 -0500

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { terra } from '../assets/terra';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'terra';
+export class AppComponent implements OnInit {
+  
+  keys: any = [];
+  titles: any = [];
+  values: any = [];
+
+  ngOnInit() {
+    Object.entries(terra).forEach(([key, value]) => {
+      this.keys = [...this.keys, key];
+      Object.entries(value).forEach(([key2, value2]) => {
+        this.titles = [...this.titles, key2];
+        this.values = [...this.values, value2];
+      });
+    })
+  }
+
 }
